ea11a9d6c368122c5467a45e78ab7404929ad65d
Barosl Lee <vcs@barosl.com>
2015-01-21 04:05:13 +0900

    Clean up the design of the queue page

--- homu/server.py
+++ homu/server.py
@@ -26,7 +26,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             rows = []
             for state in pull_states:
                 rows.append({
-                    'status': state.status if state.status else 'undecided',
+                    'status': state.status,
                     'priority': 'rollup' if state.rollup else state.priority,
                     'url': 'https://github.com/{}/{}/pull/{}'.format(repo.owner, repo.name, state.num),
                     'num': state.num,
