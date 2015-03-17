4aee503869add3a0cff67a83fc20ee7984517d69
Barosl Lee <vcs@barosl.com>
2015-03-17 22:10:53 +0900

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='homu',
-    version='0.1.0',
+    version='0.2.0',
     author='Barosl Lee',
     url='https://github.com/barosl/homu',
-    description='Homu is a continuous integration service bot for GitHub and Buildbot.',
+    description='A bot that integrates with GitHub and your favorite continuous integration service',
 
     packages=['homu'],
     install_requires=[
